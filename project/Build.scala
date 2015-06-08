8867e005154bc8740be43050e93ea7f86871f614
Niklas Klein <mail@taig.io>
2015-06-09 01:16:41 +0200

    Upgrade to android-sdk-plugin 1.4.0

--- project/Build.scala
+++ project/Build.scala
@@ -8,30 +8,60 @@ extends	android.AutoBuild
 	lazy val main = Project( "hello-scala", file( "." ) )
 		.settings(
 			// Enforce Java 7 compilation (in case you have the JDK 8 installed)
-			javacOptions ++= Seq( "-source", "1.7", "-target", "1.7" ),
-			libraryDependencies ++= Seq(
-				"com.android.support" % "appcompat-v7" % "22.2.0",
-				"com.android.support" % "cardview-v7" % "22.2.0",
-				"com.android.support" % "design" % "22.2.0",
-				"com.android.support" % "gridlayout-v7" % "22.2.0",
-				"com.android.support" % "recyclerview-v7" % "22.2.0",
-				"com.android.support" % "support-v4" % "22.2.0"
+			javacOptions ++= (
+				"-source" :: "1.7" ::
+				"-target" :: "1.7" ::
+				Nil
+			),
+			libraryDependencies ++= (
+				"com.android.support" % "appcompat-v7" % "22.2.0" ::
+				"com.android.support" % "cardview-v7" % "22.2.0" ::
+				"com.android.support" % "design" % "22.2.0" ::
+				"com.android.support" % "gridlayout-v7" % "22.2.0" ::
+				"com.android.support" % "recyclerview-v7" % "22.2.0" ::
+				"com.android.support" % "support-v4" % "22.2.0" ::
+				"com.typesafe.play" %% "play-json" % "2.3.9" ::
+				Nil
 			),
 			name := "HelloScala",
-			scalacOptions ++= Seq(
-				// Print detailed deprecation warnings to the console (Note that you can not use @SuppressWarnings in Scala)
-				"-deprecation",
+			scalacOptions ++= (
+				// Print detailed deprecation warnings to the console
+				"-deprecation" ::
 				// Print detailed feature warnings to the console
-				"-feature"
+				"-feature" ::
+				Nil
 			),
 			// Don't upgrade to 2.12.x as it requires Java 8 which does not work with Android
-			scalaVersion := "2.11.6"
+			scalaVersion := "2.11.6",
+			version := "0.0.0"
 		)
 		.settings(
+			// Prevent common com.android.builder.packaging.DuplicateFileException.
+			// Add further file names if you experience the exception after adding
+			// new dependencies
+			apkbuildExcludes in Android ++= (
+				"META-INF/LICENSE" ::
+				"META-INF/LICENSE.txt" ::
+				"META-INF/NOTICE" ::
+				"META-INF/NOTICE.txt" ::
+				Nil
+			),
 			minSdkVersion in Android := "14",
 			packageRelease <<= packageRelease in Android,
-			proguardCache in Android ++= Seq(),
-			proguardOptions in Android ++= Seq(),
+			proguardCache in Android ++= (
+				"android.support" ::
+				"play" ::
+				Nil
+			),
+			proguardOptions in Android ++= (
+				"-keepattributes EnclosingMethod,InnerClasses,Signature" ::
+				"-dontwarn org.w3c.dom.bootstrap.DOMImplementationRegistry" ::
+				"-dontwarn javax.xml.bind.DatatypeConverter" ::
+				"-dontnote org.joda.time.DateTimeZone" ::
+				"-dontnote scala.concurrent.stm.impl.STMImpl$" ::
+				Nil
+			),
+			// Shortcut: allows you to execute "sbt run" instead of "sbt android:run"
 			run <<= run in Android,
 			targetSdkVersion in Android := "22",
 			versionCode in Android := Some( 0 ),
--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -4,14 +4,16 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
 
-class Hello extends Activity with TypedActivity
+class Hello extends Activity with TypedFindView
 {
-  override def onCreate( savedInstanceState: Bundle ): Unit =
-  {
-    super.onCreate( savedInstanceState )
+	override def onCreate( savedInstanceState: Bundle ): Unit =
+	{
+		super.onCreate( savedInstanceState )
 
-    setContentView( R.layout.main )
+		setContentView( R.layout.main )
 
-    findView( TR.welcome ).setText( "Hello Scala!" )
-  }
+		play.api.libs.json.Json.parse( "3" )
+
+		findView( TR.welcome ).setText( "Hello Scala!" )
+	}
 }
