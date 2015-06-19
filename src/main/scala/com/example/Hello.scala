70dcd7dafd828ea90460eb10c30e163ac42028fd
Niklas Klein <mail@taig.io>
2015-06-19 17:15:12 +0200

    Add actionbar and few features to main activity

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,6 +2,7 @@ package com.example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import android.widget.TextView
 
 class Hello extends AppCompatActivity with TypedFindView
@@ -12,6 +13,19 @@ class Hello extends AppCompatActivity with TypedFindView
 
 		setContentView( R.layout.main )
 
+		setSupportActionBar( findView( TR.toolbar ) )
+		getSupportActionBar.setDisplayHomeAsUpEnabled( true )
+
 		findView( TR.welcome ).setText( R.string.name )
+	}
+
+	override def onOptionsItemSelected( item: MenuItem ) = item.getItemId match
+	{
+		case android.R.id.home =>
+		{
+			finish()
+			true
+		}
+		case _ => super.onOptionsItemSelected( item )
 	}
 }
