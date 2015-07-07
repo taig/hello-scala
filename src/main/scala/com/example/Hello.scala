3fc271ef9048202ba32666a9f3bc4e4667553faa
Niklas Klein <mail@taig.io>
2015-07-07 23:05:42 +0200

    Remove unused import

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -3,7 +3,6 @@ package com.example
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-import android.widget.TextView
 
 class Hello extends AppCompatActivity with TypedFindView
 {
