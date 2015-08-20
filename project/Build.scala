c6cb4e86c6f3490e4740921da7a21dd434346e2e
Niklas Klein <mail@taig.io>
2015-08-20 10:47:25 +0200

    Upgrade to com.android.support.* 23.0.0

--- project/Build.scala
+++ project/Build.scala
@@ -17,12 +17,12 @@ extends	android.AutoBuild
 				Nil
 			),
 			libraryDependencies ++= (
-				"com.android.support" % "appcompat-v7" % "22.2.1" ::
-				"com.android.support" % "cardview-v7" % "22.2.1" ::
-				"com.android.support" % "design" % "22.2.1" ::
-				"com.android.support" % "gridlayout-v7" % "22.2.1" ::
-				"com.android.support" % "recyclerview-v7" % "22.2.1" ::
-				"com.android.support" % "support-v4" % "22.2.1" ::
+				"com.android.support" % "appcompat-v7" % "23.0.0" ::
+				"com.android.support" % "cardview-v7" % "23.0.0" ::
+				"com.android.support" % "design" % "23.0.0" ::
+				"com.android.support" % "gridlayout-v7" % "23.0.0" ::
+				"com.android.support" % "recyclerview-v7" % "23.0.0" ::
+				"com.android.support" % "support-v4" % "23.0.0" ::
 				// Version 2.4.x requires Java 8
 				"com.typesafe.play" %% "play-json" % "2.3.10" ::
 				Nil
